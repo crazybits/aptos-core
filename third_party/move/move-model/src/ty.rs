4a691233b5f97fd570ede905333f58e93abdfbbe
Brian R. Murphy <132495859+brmataptos@users.noreply.github.com>
2024-10-09 17:32:18 -0700

    add test showing Issue #14913 and a simple fix

--- third_party/move/move-model/src/ty.rs
+++ third_party/move/move-model/src/ty.rs
@@ -3264,6 +3264,8 @@ pub struct TypeDisplayContext<'a> {
     pub used_modules: BTreeSet<ModuleId>,
     /// Whether to use `m::T` for representing types, for stable output in docgen
     pub use_module_qualification: bool,
+    /// Var type that should appear as Self in display
+    pub opt_self_var: Option<u32>,
 }
 
 impl<'a> TypeDisplayContext<'a> {
@@ -3277,6 +3279,7 @@ impl<'a> TypeDisplayContext<'a> {
             display_type_vars: false,
             used_modules: BTreeSet::new(),
             use_module_qualification: false,
+            opt_self_var: None,
         }
     }
 
@@ -3300,6 +3303,7 @@ impl<'a> TypeDisplayContext<'a> {
             display_type_vars: false,
             used_modules: BTreeSet::new(),
             use_module_qualification: false,
+            opt_self_var: None,
         }
     }
 
@@ -3307,6 +3311,13 @@ impl<'a> TypeDisplayContext<'a> {
         Self {
             subs_opt: Some(subs),
             ..self
+        }
+    }
+
+    pub fn map_var_to_self(&self, idx: u32) -> Self {
+        Self {
+            opt_self_var: Some(idx),
+            ..self.clone()
         }
     }
 }
@@ -3400,6 +3411,11 @@ impl<'a> fmt::Display for TypeDisplay<'a> {
                 }
             },
             Var(idx) => {
+                if let Some(idx2) = self.context.opt_self_var {
+                    if *idx == idx2 {
+                        return f.write_str("Self");
+                    }
+                }
                 if let Some(ty) = self.context.subs_opt.and_then(|s| s.subs.get(idx)) {
                     write!(f, "{}", ty.display(self.context))
                 } else if let Some(ctrs) =
@@ -3409,9 +3425,10 @@ impl<'a> fmt::Display for TypeDisplay<'a> {
                     if ctrs.is_empty() {
                         f.write_str(&self.type_var_str(*idx))
                     } else {
+                        let recursive_context = self.context.map_var_to_self(*idx);
                         let out = ctrs
                             .iter()
-                            .map(|(_, _, c)| c.display(self.context).to_string())
+                            .map(|(_, _, c)| c.display(&recursive_context).to_string())
                             .join(" + ");
                         f.write_str(&out)
                     }
